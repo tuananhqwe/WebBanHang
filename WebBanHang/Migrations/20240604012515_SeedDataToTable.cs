04f58faf42f94378d9e3b026b7a57ed28b3db068
ADMIN <ADMIN@PC233>
2024-06-07 11:00:09 +0700

    Add project files.

---
+++ WebBanHang/Migrations/20240604012515_SeedDataToTable.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebBanHang.Migrations
+{
+    public partial class SeedDataToTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
